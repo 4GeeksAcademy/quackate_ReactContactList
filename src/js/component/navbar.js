712c4ce297cf09460ed566f3928c63282da72f1d
Kate Deras <65469487+quackate@users.noreply.github.com>
2023-08-02 17:42:09 +0000

    Styling Changes Made

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/index.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-				<h3 className="mb-0 ms-3">Contact List <i className="far fa-address-book"></i></h3>
+		<nav className="navbar navbar-light bg-light mb-3 py-3">
+				<h3 className="mb-0 ms-4">Contact List <i className="far fa-address-book"></i></h3>
 			<div className="ml-auto">
 				<Link to="/addcontact">
-					<button className="btn btn-success me-3">Add new contact</button>
+					<button className="contact-button btn me-4">Add new contact</button>
 				</Link>
 			</div>
 		</nav>
